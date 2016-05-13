b0f4849add02789b4683abb868669cf60e1d163f
yufen yu <yuyufen2008@sina.com>
2016-05-13 08:51:48 +0800

    lab2 part1

--- kern/pmap.c
+++ kern/pmap.c
@@ -81,6 +81,7 @@ static void *
 boot_alloc(uint32_t n)
 {
 	static char *nextfree;	// virtual address of next byte of free memory
+	static int nAvailPages;
 	char *result;
 
 	// Initialize nextfree if this is the first time.
@@ -90,6 +91,7 @@ boot_alloc(uint32_t n)
 	// to any kernel code or global variables.
 	if (!nextfree) {
 		extern char end[];
+		nAvailPages = npages;
 		nextfree = ROUNDUP((char *) end, PGSIZE);
 	}
 
@@ -98,7 +100,14 @@ boot_alloc(uint32_t n)
 	// to a multiple of PGSIZE.
 	//
 	// LAB 2: Your code here.
-
+	char *res = nextfree;
+	if(n) {
+		if(n / PGSIZE > nAvailPages) panic("out of memory!\n");
+		nextfree += n;
+		nextfree = ROUNDUP(nextfree, PGSIZE);
+		nAvailPages -= (nextfree - res) / PGSIZE;
+	}
+	return res;
 	return NULL;
 }
 
@@ -121,7 +130,7 @@ mem_init(void)
 	i386_detect_memory();
 
 	// Remove this line when you're ready to test this function.
-	panic("mem_init: This function is not finished\n");
+	//panic("mem_init: This function is not finished\n");
 
 	//////////////////////////////////////////////////////////////////////
 	// create initial page directory.
@@ -144,7 +153,8 @@ mem_init(void)
 	// array.  'npages' is the number of physical pages in memory.  Use memset
 	// to initialize all fields of each struct PageInfo to 0.
 	// Your code goes here:
-
+	pages = (struct PageInfo *)boot_alloc(sizeof(struct PageInfo) * npages);
+	memset(pages, 0, sizeof(struct PageInfo) * npages);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -155,6 +165,7 @@ mem_init(void)
 	page_init();
 
 	check_page_free_list(1);
+panic("page_init(): OK\n");
 	check_page_alloc();
 	check_page();
 
@@ -249,6 +260,11 @@ page_init(void)
 	// free pages!
 	size_t i;
 	for (i = 0; i < npages; i++) {
+		if(i == 0) continue;
+		/* BIOS */
+		if(i >= PGNUM(IOPHYSMEM) && i < PGNUM(EXTPHYSMEM)) continue;
+		/* kernel and pgdir */
+		if(i >= PGNUM(KERNBASE) && i < PGNUM(PADDR(boot_alloc(0)))) continue;
 		pages[i].pp_ref = 0;
 		pages[i].pp_link = page_free_list;
 		page_free_list = &pages[i];
@@ -271,7 +287,15 @@ struct PageInfo *
 page_alloc(int alloc_flags)
 {
 	// Fill this function in
-	return 0;
+	struct PageInfo *res = page_free_list;
+	res->pp_link = NULL;
+	page_free_list = page_free_list->pp_link;
+	
+	if(alloc_flags & ALLOC_ZERO) {
+		memset(page2kva(res), 0, PGSIZE);
+	}
+	
+	return res;
 }
 
 //
@@ -284,6 +308,10 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
+	assert(pp->pp_ref == 0);
+	assert(pp->pp_link == NULL);
+	pp->pp_link = page_free_list;
+	page_free_list = pp;
 }
 
 //
